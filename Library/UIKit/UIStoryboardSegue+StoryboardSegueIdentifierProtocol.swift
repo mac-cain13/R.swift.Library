0ab9e81361dc5ed8919ffd49e31d6b789882739d
Mathijs Kadijk <mkadijk@gmail.com>
2015-12-12 23:27:20 +0100

    SwiftDoc for UIStoryboardSegue

--- Library/UIKit/UIStoryboardSegue+StoryboardSegueIdentifierProtocol.swift
+++ Library/UIKit/UIStoryboardSegue+StoryboardSegueIdentifierProtocol.swift
@@ -10,6 +10,13 @@ import Foundation
 import UIKit
 
 public extension UIStoryboardSegue {
+  /**
+   Get typed info about this segue for the given identifier (R.segue.*)
+   
+   - parameter identifier: The identifier this segue should be an instance of.
+   
+   - returns: Types information such as the destinationViewController cast to the correct type or nil if the segue doesn't match the given identifier (R.segue.*)
+  */
   public func typedInfoWithIdentifier<Identifier: StoryboardSegueIdentifierProtocol, Segue, Source, Destination where Segue == Identifier.SegueType, Source == Identifier.SourceType, Destination == Identifier.DestinationType>(identifier: Identifier) -> TypedStoryboardSegueInfo<Segue, Source, Destination>? {
     guard self.identifier == identifier.identifier else { return nil }
     return TypedStoryboardSegueInfo(segue: self)

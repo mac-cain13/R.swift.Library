ec518cccddcfd2d110af34fe3c4e8fd76db7d70c
Mathijs Kadijk <mkadijk@gmail.com>
2015-12-12 23:44:46 +0100

    SwiftDoc for StoryboardSegueIdentifierProtocol

--- Library/Core/StoryboardSegueIdentifierProtocol.swift
+++ Library/Core/StoryboardSegueIdentifierProtocol.swift
@@ -8,31 +8,62 @@
 
 import Foundation
 
+/// Segue identifier protocol
 public protocol StoryboardSegueIdentifierProtocol: Identifier {
+  /// Type of the segue itself
   typealias SegueType
+
+  /// Type of the source view controller
   typealias SourceType
+
+  /// Type of the destination view controller
   typealias DestinationType
 }
 
+/// Segue identifier
 public struct StoryboardSegueIdentifier<Segue, Source, Destination>: StoryboardSegueIdentifierProtocol {
+  /// Type of the segue itself
   public typealias SegueType = Segue
+
+  /// Type of the source view controller
   public typealias SourceType = Source
+
+  /// Type of the destination view controller
   public typealias DestinationType = Destination
 
+  /// Identifier string of this segue
   public let identifier: String
 
+  /**
+   Create a new identifier based on the identifier string
+   
+   - returns: A new StoryboardSegueIdentifier
+  */
   public init(identifier: String) {
     self.identifier = identifier
   }
 }
 
+/// Typed segue information
 public struct TypedStoryboardSegueInfo<Segue, Source, Destination>: StoryboardSegueIdentifierProtocol {
+  /// Type of the segue itself
   public typealias SegueType = Segue
+
+  /// Type of the source view controller
   public typealias SourceType = Source
+
+  /// Type of the destination view controller
   public typealias DestinationType = Destination
 
+  /// Segue destination view controller
   public let destinationViewController: Destination
+
+  /// Segue identifier
   public let identifier: String
+
+  /// The original segue
   public let segue: Segue
+
+  /// Segue source view controller
   public let sourceViewController: Source
 }

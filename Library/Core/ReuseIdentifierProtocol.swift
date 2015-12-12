564a4bac499611c5efbe25b99d8b7ec4fb95b6e7
Mathijs Kadijk <mkadijk@gmail.com>
2015-12-12 23:48:46 +0100

    SwiftDoc for ReuseIdentifierProtocol

--- Library/Core/ReuseIdentifierProtocol.swift
+++ Library/Core/ReuseIdentifierProtocol.swift
@@ -8,15 +8,27 @@
 
 import Foundation
 
+/// Reuse identifier protocol
 public protocol ReuseIdentifierProtocol: Identifier {
+  /// Type of this reuseable
   typealias ReusableType
 }
 
+/// Reuse identifier
 public struct ReuseIdentifier<Reusable>: ReuseIdentifierProtocol {
+  /// Type of this reuseable
   public typealias ReusableType = Reusable
 
+  /// String identifier of this reusable
   public let identifier: String
 
+  /**
+   Create a new ReuseIdentifier based on the string identifier
+   
+   - parameter identifier: The string identifier for this reusable
+
+   - returns: A new ReuseIdentifier
+  */
   public init(identifier: String) {
     self.identifier = identifier
   }

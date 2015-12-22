7d145f501e5fb4f539cc6d87229d86e8b52cbb4e
Mathijs Kadijk <mkadijk@gmail.com>
2015-12-22 22:42:23 +0100

    Implement initialize and instantiate nib methods
    in extension.

--- Library/Core/NibResource.swift
+++ Library/Core/NibResource.swift
@@ -16,11 +16,27 @@ public protocol NibResource {
 
   /// Name of the nib file on disk
   var name: String { get }
+}
 
+public extension NibResource {
   /**
    Create a new instance of this nib file
-   
+
    - returns: A new instance of this nib
-  */
-  func initialize() -> UINib
+   */
+  public func initialize() -> UINib {
+    return UINib.init(nibName: name, bundle: bundle)
+  }
+
+  /**
+   Instantiate the nib to get the top-level object from this nib
+
+   - parameter ownerOrNil: The owner, if the owner parameter is nil, connections to File's Owner are not permitted.
+   - parameter options: Options are identical to the options specified with -[NSBundle loadNibNamed:owner:options:]
+
+   - returns: An array containing the top-level objects from the NIB
+   */
+  public func instantiateWithOwner(ownerOrNil: AnyObject?, options optionsOrNil: [NSObject : AnyObject]?) -> [AnyObject] {
+    return initialize().instantiateWithOwner(ownerOrNil, options: optionsOrNil)
+  }
 }

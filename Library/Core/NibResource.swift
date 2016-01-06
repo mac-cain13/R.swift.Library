158aa468659df88cdb432f961d91ed2b70a7c7e3
Mathijs Kadijk <mkadijk@gmail.com>
2016-01-06 08:57:13 +0100

    Amendment to UIKit fix

--- Library/Core/NibResource.swift
+++ Library/Core/NibResource.swift
@@ -17,17 +17,3 @@ public protocol NibResource {
   /// Name of the nib file on disk
   var name: String { get }
 }
-
-public extension NibResource {
-  /**
-   Instantiate the nib to get the top-level object from this nib
-
-   - parameter ownerOrNil: The owner, if the owner parameter is nil, connections to File's Owner are not permitted.
-   - parameter options: Options are identical to the options specified with -[NSBundle loadNibNamed:owner:options:]
-
-   - returns: An array containing the top-level objects from the NIB
-   */
-  public func instantiateWithOwner(ownerOrNil: AnyObject?, options optionsOrNil: [NSObject : AnyObject]?) -> [AnyObject] {
-    return initialize().instantiateWithOwner(ownerOrNil, options: optionsOrNil)
-  }
-}
--- Library/UIKit/NibResource+UIKit.swift
+++ Library/UIKit/NibResource+UIKit.swift
@@ -17,4 +17,16 @@ public extension NibResource {
   public func initialize() -> UINib {
     return UINib.init(nibName: name, bundle: bundle)
   }
+
+  /**
+   Instantiate the nib to get the top-level object from this nib
+
+   - parameter ownerOrNil: The owner, if the owner parameter is nil, connections to File's Owner are not permitted.
+   - parameter options: Options are identical to the options specified with -[NSBundle loadNibNamed:owner:options:]
+
+   - returns: An array containing the top-level objects from the NIB
+   */
+  public func instantiateWithOwner(ownerOrNil: AnyObject?, options optionsOrNil: [NSObject : AnyObject]?) -> [AnyObject] {
+    return initialize().instantiateWithOwner(ownerOrNil, options: optionsOrNil)
+  }
 }

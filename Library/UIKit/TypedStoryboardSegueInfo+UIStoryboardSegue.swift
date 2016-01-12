0adc907de880aabeefaf1f8dd2f243ef1874e9b4
Mathijs Kadijk <mkadijk@gmail.com>
2016-01-12 10:32:04 +0100

    Also take StoryboardSegueIdentifierType in init
    So TypedStoryboardSegueInfo "types itself"

--- Library/UIKit/TypedStoryboardSegueInfo+UIStoryboardSegue.swift
+++ Library/UIKit/TypedStoryboardSegueInfo+UIStoryboardSegue.swift
@@ -15,11 +15,12 @@ extension TypedStoryboardSegueInfo {
    
    - returns: A newly initialized TypedStoryboardSegueInfo object or nil.
   */
-  public init?(segue: UIStoryboardSegue) {
+  public init?<SegueIdentifier: StoryboardSegueIdentifierType where SegueIdentifier.SegueType == Segue, SegueIdentifier.SourceType == Source, SegueIdentifier.DestinationType == Destination>(segueIdentifier: SegueIdentifier, segue: UIStoryboardSegue) {
     guard let identifier = segue.identifier,
-      sourceViewController = segue.sourceViewController as? Source,
-      destinationViewController = segue.destinationViewController as? Destination,
-      segue = segue as? Segue
+      sourceViewController = segue.sourceViewController as? SegueIdentifier.SourceType,
+      destinationViewController = segue.destinationViewController as? SegueIdentifier.DestinationType,
+      segue = segue as? SegueIdentifier.SegueType
+      where identifier == segueIdentifier.identifier
       else {
         return nil
     }

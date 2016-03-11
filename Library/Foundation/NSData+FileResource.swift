7d45067a420e50d450b109ea9478fa96278bbd2e
Tom Lokhorst <tom@lokhorst.eu>
2016-03-11 09:22:48 +0100

    Add NSData.init(resource:) for initializing NSData with R.file.* resources

---
+++ Library/Foundation/NSData+FileResource.swift
@@ -0,0 +1,29 @@
+//
+//  NSData+FileResource.swift
+//  R.swift.Library
+//
+//  Created by Tom Lokhorst on 2016-03-11.
+//  Copyright © 2016 Mathijs Kadijk. All rights reserved.
+//
+
+import Foundation
+
+public extension NSData {
+
+  /**
+   Creates and returns NSData with the contents of the specified file resource (R.file.*).
+
+   - parameter resource: The file resource (R.file.*)
+
+   - returns: A NSData object with the contents of the specified file.
+   */
+  public convenience init?(resource: FileResourceType) {
+    let bundle = resource.bundle ?? NSBundle.mainBundle()
+
+    guard let url = bundle.URLForResource(resource.name, withExtension: resource.pathExtension) else {
+      return nil
+    }
+
+    self.init(contentsOfURL: url)
+  }
+}

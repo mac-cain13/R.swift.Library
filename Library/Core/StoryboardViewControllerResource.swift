33d575484ad9a3938575eda05a52b82b8d33f563
Mathijs Kadijk <mkadijk@gmail.com>
2016-03-13 15:56:36 +0100

    Add StoryboardViewControllerResourceType
    Including a helper method: instantiateViewController

---
+++ Library/Core/StoryboardViewControllerResource.swift
@@ -0,0 +1,25 @@
+//
+//  StoryboardViewControllerResource.swift
+//  R.swift.Library
+//
+//  Created by Mathijs Kadijk on 13-03-16.
+//  Copyright © 2016 Mathijs Kadijk. All rights reserved.
+//
+
+import Foundation
+
+public protocol StoryboardViewControllerResourceType: IdentifierType {
+  typealias ViewControllerType
+}
+
+public struct StoryboardViewControllerResource<ViewController>: StoryboardViewControllerResourceType {
+  public typealias ViewControllerType = ViewController
+
+  /// Storyboard identifier of this view controller
+  public let identifier: String
+
+  public init(identifier: String) {
+    self.identifier = identifier
+  }
+}
+

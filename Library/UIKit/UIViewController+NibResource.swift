a560db90ece07f7279bd15cd5e45e997d63df54e
Mathijs Kadijk <mkadijk@gmail.com>
2016-03-13 15:56:06 +0100

    Correct comment about UIViewController nib loading

--- Library/UIKit/UIViewController+NibResource.swift
+++ Library/UIKit/UIViewController+NibResource.swift
@@ -13,7 +13,7 @@ public extension UIViewController {
   /**
    Returns a newly initialized view controller with the nib resource (R.nib.*).
    
-   - parameter nib: The name of the nib resource (R.nib.*) to associate with the view controller.
+   - parameter nib: The nib resource (R.nib.*) to associate with the view controller.
    
    - returns: A newly initialized UIViewController object.
   */
